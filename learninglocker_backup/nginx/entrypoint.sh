16649d2632de868c9dac7dffa890940757cc39c4
PiAir <PiAir@users.noreply.github.com>
2021-05-06 21:26:46 +0200

    fixed Learninglocker setup

--- learninglocker_backup/nginx/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# substitute env variables in configuration
-export DOLLAR='$'
-envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-exec "$@"
-
