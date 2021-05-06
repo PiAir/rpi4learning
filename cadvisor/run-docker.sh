e72d7b18aef5194a6c59bb267c0f78d78ff3074b
PiAir <PiAir@users.noreply.github.com>
2021-05-06 12:42:36 +0200

    fixes

--- cadvisor/run-docker.sh
+++ cadvisor/run-docker.sh
@@ -1,1 +1,1 @@
-docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro --publish=8081:8080 --detach=true --network=rpi4learning --name=cadvisor google/cadvisor:latest
+docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro --publish=8081:8080 --detach=true --network=rpi4learning --name=cadvisor budry/cadvisor-arm:latest
--- phpmyadmin/run-docker.sh
+++ phpmyadmin/run-docker.sh
@@ -1,1 +1,1 @@
-docker run --name phpmyadmin -d -e PMA_ARBITRARY=1 -p 8080:80 --network=rpi4learning phpmyadmin
+docker run --name phpmyadmin -d -e PMA_HOST=mariadb -p 8080:80 --network=rpi4learning phpmyadmin
