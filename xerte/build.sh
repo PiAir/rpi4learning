f95e7e90e44864631a3445becc01a54016d72d27
PiAir <PiAir@users.noreply.github.com>
2021-05-08 14:05:42 +0200

    added folders

--- xerte/build.sh
+++ xerte/build.sh
@@ -1,1 +1,2 @@
-docker build --build-arg XERTE_VERSION=3.9 --build-arg TSUGI_VERSION=21.04.24 -t rpi4learningbox/xerte:latest . 
+docker build --build-arg XERTE_VERSION=3.9 --build-arg TSUGI_VERSION=21.04.24 -t rpi4learningbox/xerte:3.9 . 
+
