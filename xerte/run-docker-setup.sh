9d75ed31280a80b70e7cfcdf4c89854f1a5c3cb0
PiAir <PiAir@users.noreply.github.com>
2021-07-10 11:00:33 +0200

    rpi4learningbox/xerte:3.10

--- xerte/run-docker-setup.sh
+++ xerte/run-docker-setup.sh
@@ -1,2 +1,2 @@
 chmod 0777 $PWD/USER-FILES
-docker run -td --name=xerte -p 8041:80 --network=rpi4learning --restart=unless-stopped -v $PWD/php.ini:/usr/local/etc/php/php.ini -v $PWD/USER-FILES:/var/www/html/xerteonlinetoolkits/USER-FILES rpi4learningbox/xerte:3.9
+docker run -td --name=xerte -p 8041:80 --network=rpi4learning --restart=unless-stopped -v $PWD/php.ini:/usr/local/etc/php/php.ini -v $PWD/USER-FILES:/var/www/html/xerteonlinetoolkits/USER-FILES rpi4learningbox/xerte:3.10
