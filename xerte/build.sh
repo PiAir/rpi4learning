ffdb88fb4411baeed48751717340706fab6525d4
PiAir <PiAir@users.noreply.github.com>
2021-07-10 10:59:21 +0200

    XERTE_VERSION=3.10

--- xerte/build.sh
+++ xerte/build.sh
@@ -1,2 +1,2 @@
-docker build --build-arg XERTE_VERSION=3.9 --build-arg TSUGI_VERSION=21.04.24 -t rpi4learningbox/xerte:3.9 . 
+docker build --build-arg XERTE_VERSION=3.10 --build-arg TSUGI_VERSION=21.04.24 -t rpi4learningbox/xerte:3.10 . 
 
