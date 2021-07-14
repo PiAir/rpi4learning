439590321bd5a88ab07600a1bc9424ee90a0760b
PiAir <PiAir@users.noreply.github.com>
2021-07-14 21:30:46 +0200

    first commit of build script	new file:   build.sh empty folder for config	new file:   heimdall/config/.keep heimdall first commit	new file:   heimdall/docker-compose.yml

---
+++ build.sh
@@ -0,0 +1,9 @@
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo apt-get install -y libffi-dev libssl-dev 
+sudo apt install -y python3-dev
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install docker-compose
+
+docker --version
+docker-compose --version
