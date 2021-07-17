d65aa6e1f6028207e6b6bb322348db75f72fcda5
PiAir <PiAir@users.noreply.github.com>
2021-07-17 11:47:13 +0200

    added usermod pi

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+sudo usermod -aG docker pi
 sudo apt-get install -y libffi-dev libssl-dev 
 sudo apt install -y python3-dev
 sudo apt-get install -y python3 python3-pip
