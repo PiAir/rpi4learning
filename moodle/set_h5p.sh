29e6ce15448f1f63f8f55374b603a107dfe62f09
PiAir <PiAir@users.noreply.github.com>
2021-07-18 14:50:50 +0200

    fixed export statement

--- moodle/set_h5p.sh
+++ moodle/set_h5p.sh
@@ -1,3 +1,3 @@
-# export $(cat .env | xargs)
+export $(cat .env | xargs)
 sed -i 's|{LRS_URL}|'"$LRS_URL"'|' /home/pi/docker/moodle/xapi-h5p/js/xapi-stmt-dispatcher.js
 sed -i 's|{LRS_USERNAME_PASSWORD}|'"$LRS_USERNAME:$LRS_PASSWORD"'|' /home/pi/docker/moodle/xapi-h5p/js/xapi-stmt-dispatcher.js
