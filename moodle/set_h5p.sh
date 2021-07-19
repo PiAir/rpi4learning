82381a6dec556587aba3ce4a309b50297026e25b
PiAir <PiAir@users.noreply.github.com>
2021-07-19 12:46:03 +0200

    Added cp

--- moodle/set_h5p.sh
+++ moodle/set_h5p.sh
@@ -1,3 +1,4 @@
 export $(cat .env | xargs)
+cp /home/pi/docker/moodle/xapi-h5p/js/xapi-stmt-dispatcher-template.js /home/pi/docker/moodle/xapi-h5p/js/xapi-stmt-dispatcher.js
 sed -i 's|{LRS_URL}|'"$LRS_URL"'|' /home/pi/docker/moodle/xapi-h5p/js/xapi-stmt-dispatcher.js
 sed -i 's|{LRS_USERNAME_PASSWORD}|'"$LRS_USERNAME:$LRS_PASSWORD"'|' /home/pi/docker/moodle/xapi-h5p/js/xapi-stmt-dispatcher.js
