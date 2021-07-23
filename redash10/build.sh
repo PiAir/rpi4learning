d54764e80b0a8f5bd50b49ab82330b18d5b2c6b2
PiAir <PiAir@users.noreply.github.com>
2021-07-23 16:09:05 +0200

    cp custom requirements files

--- redash10/build.sh
+++ redash10/build.sh
@@ -3,8 +3,8 @@ unzip *.zip
 rm *.zip
 mv redash-10.0.0-beta app
 cp Dockerfile app/Dockerfile
-# cp requirements.txt app/requirements.txt
-# cp requirements_all_ds.txt app/requirements_all_ds.txt
+cp requirements.txt app/requirements.txt
+cp requirements_all_ds_2.txt app/requirements_all_ds.txt
 
 docker build -t rpi4learningbox/redash:10.0.0-beta.b49597 app
 docker build -t rpi4learningbox/redash-nginx:1.20.0 nginx
